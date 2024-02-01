b5cc7f51b32c1d8ebd9ed9016fa746cc6d9c935b
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-02-01 19:55:42 +0200

    add hover to cart project

--- src/components/CartProjects/CartProjects.tsx
+++ src/components/CartProjects/CartProjects.tsx
@@ -20,20 +20,30 @@ export type ICartProjectsProps = {
 export default function CartProjects({item}:ICartProjectsProps) {
   const {id, title, subTitle,description, image, pathToGitHub, pathToLivePage, tag} = item;
   return (
-    <div className="hover:bg-gradient-to-r from-[#e9a16b] from-[4.61%] via-[rgba(254, 226, 205, 0.6)] via-[48.56%] to-[rgba(254, 255, 193, 0.4)] to-[88.73%]
+    <div className="group">
+    <div className=" h-full group-hover:bg-gradient-to-r from-[#e9a16b] from-[4.61%] via-[rgba(254, 226, 205, 0.6)] via-[48.56%] to-[rgba(254, 255, 193, 0.4)] to-[88.73%]
      flex flex-col gap-4 items-center xl:gap-[30px] px-4 py-4 xl:px-[40px] xl:py-[40px] rounded-[10px] overflow-hidden border-[1px] border-solid border-color-btn-extra">
-        {/* <Image src={phonebook} alt='Phonebook' width={500} height={400}
-        className="object-contain w-[300px] h-[180px] xl:w-[500px] xl:h-[400px] rounded-[10px] overflow-hidden" /> */}
-        <div style={{background: `url(${phonebook})`, backgroundSize: 'cover'}} className="w-[300px] h-[180px] xl:w-[500px] xl:h-[400px] rounded-[10px] overflow-hidden"></div>
-        <div className="flex flex-col gap-2 xl:gap-4">
-          <h2 className="font-roboto text-xl font-bold leading-6 xl:leading-[28.8px]">{title}</h2>
-          <p className="font-condensed text-color-accent-primary text-base font-medium leading-[19.2px] xl:text-xl xl:font-normal xl:leading-6">{subTitle}</p>
-          {/* <div className="text-[14px] leading-[21px] xl:text-2xl xl:leading-[31.2px]">This is a simple phone book that utilizes the Swagger backend API. It allows users to register and log in, add, delete, and filter contacts, as well as log out. Role: Developer.</div> */}
-          <div className="flex justify-center gap-2 xl:gap-4 xl:justify-end">
+        <Image src={image} alt={title} width={500} height={400}
+        className="object-contain w-[300px] xl:w-[500px] rounded-[10px] overflow-hidden
+        relative transform transition-all duration-500 opacity-100 scale-y-100 group-hover:scale-y-90" />
+        
+        <div className="h-[120px] group-hover:h-[200px] md:h-[163px] group-hover:md:h-[220px] xl:h-[200px]  group-hover:xl:h-[328px] transition-all duration-500 w-full flex flex-col justify-between">
+          <div className="relative flex flex-col gap-2 xl:gap-4">
+            <h2 className="transition-all duration-500 opacity-100 group-hover:opacity-0 font-roboto text-xl font-bold leading-6 xl:leading-[28.8px]">{title}</h2>
+            <p className="transition-all duration-500 opacity-100 group-hover:opacity-0 font-condensed text-color-accent-primary text-base font-medium leading-[19.2px] xl:text-xl xl:font-normal xl:leading-6">{subTitle}</p>
+          
+          <div className="flex w-full  bg-transparent overlay absolute top-0 left-0 transition-all duration-500 transform translate-y-[100%] group-hover:translate-y-0 opacity-0 group-hover:opacity-100 ">
+              <p className="text-[14px] leading-[21px] xl:text-2xl xl:leading-[31.2px]">{description}</p>
+            </div>
+          </div>
+          
+          
+          <div className="relative z-30 flex justify-center gap-2 xl:gap-4 xl:justify-end">
               <Buttons title={'GitHub'} icon={'ic'} path={'/'} />
               <Buttons title={'See more'} icon={'ic'} path={'/'}/>
           </div>
         </div>
+    </div>
     </div>
   )
 }
--- src/utils/Data/projectsList.tsx
+++ src/utils/Data/projectsList.tsx
@@ -1,56 +1,59 @@
 import { Project } from "../types";
-// import carp from './../../../public/images/carp-travel.png';
-// import kapustf from '../../../public/images/phonebook.jpg';
-// import labrary from '../../../public/images/labrary.png';
-// import mobileApp from '../../../public/images/mobile-app.png';
+import carp from './../../../public/images/carp-travel.png';
+import kapustf from '../../../public/images/phonebook.jpg';
+import labrary from '../../../public/images/labrary.png';
+import mobileApp from '../../../public/images/mobile-app.png';
 
- export const projectsList:Project[] =[{
+export const projectsList: Project[] = [
+  {
     id: 1,
-    title:'Kapusta',
-    subTitle:'React (Hooks, Routers, Redux, Redux Toolkit), Styled Component, Axios',
-    description:'This is a team project in which I was responsible for implementing the following components: expense and income reporting. Specifically, the ability to enter data into a table, delete data according to categories on a selected day. Additionally, the application is responsive, adapting to phone, tablet, and computer screen extensions. Role: Develope',
-    image:"/public/images/phonebook.jpg'",
-    pathToGitHub:"/", 
-    pathToLivePage:"/",
-    tag: ['All', 'Pet'],
-},
-{
+    title: "Kapusta",
+    subTitle:
+      "React (Hooks, Routers, Redux, Redux Toolkit), Styled Component, Axios",
+    description:
+      "This is a team project in which I was responsible for implementing the following components: expense and income reporting. Specifically, the ability to enter data into a table, delete data according to categories on a selected day. Additionally, the application is responsive, adapting to phone, tablet, and computer screen extensions. Role: Develope",
+    image: "/images/phonebook.jpg",
+    pathToGitHub: "/",
+    pathToLivePage: "/",
+    tag: ["All", "Pet"],
+  },
+  {
     id: 2,
-    title:'Carp Trevel',
-    subTitle:'Next.js Tailwind CSS, TypeScript',
-    description:"",
-    image: '/public/images/carp-travel.png',
-    pathToGitHub:"/", 
-    pathToLivePage:"/",
-    tag: ['All', 'Pet'],
-},
-{
+    title: "Carp Trevel",
+    subTitle: "Next.js Tailwind CSS, TypeScript",
+    description: "This is a simple phone book that utilizes the Swagger backend API.",
+    image: "/images/carp-travel.png",
+    pathToGitHub: "/",
+    pathToLivePage: "/",
+    tag: ["All", "Pet"],
+  },
+  {
     id: 3,
-    title:'Phonebook',
-    subTitle:'React , Redux Toolkit, SASS, Axios',
-    description:"",
-    image:"/public/images/phonebook.jpg",
-    pathToGitHub:"/", 
-    pathToLivePage:"/",
-    tag: ['All', 'Com'],
-},
-{
+    title: "Phonebook",
+    subTitle: "React , Redux Toolkit, SASS, Axios",
+    description: " It allows users to register and log in, add, delete, and filter contacts, as well as log out. Role: Developer.",
+    image: "/images/phonebook.jpg",
+    pathToGitHub: "/",
+    pathToLivePage: "/",
+    tag: ["All", "Com"],
+  },
+  {
     id: 4,
-    title:'mobile-app',
-    subTitle:'HTML, CSS, JS, SASS',
-    description:"",
-    image:"/public/images/mobile-app.png",
-    pathToGitHub:"/", 
-    pathToLivePage:"/",
-    tag: ['All', 'Com'],
-},
-]
+    title: "mobile-app",
+    subTitle: "HTML, CSS, JS, SASS",
+    description: " It allows users to register and log in, add, delete, and filter contacts, as well as log out. Role: Developer. It allows users to register and log in, add, delete, and filter contacts, as well as log out. Role: Developer. It allows users to register and log in, add, delete, and filter contacts, as well as log out. Role: Developer.",
+    image: "/images/mobile-app.png",
+    pathToGitHub: "/",
+    pathToLivePage: "/",
+    tag: ["All", "Com"],
+  },
+];
 // {
 //     title:'',
 //     subTitle:'',
 //     description:"",
 //     image:"",
-//     pathToGitHub:"", 
+//     pathToGitHub:"",
 //     pathToLivePage:"",
 // },
 // {
@@ -58,6 +61,6 @@ import { Project } from "../types";
 //     subTitle:'',
 //     description:"",
 //     image:"",
-//     pathToGitHub:"", 
+//     pathToGitHub:"",
 //     pathToLivePage:"",
 // }
