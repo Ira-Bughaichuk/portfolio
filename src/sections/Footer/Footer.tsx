f45e69541de7f02f0b61ddbe7851ee53b2c6ab61
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-02-10 13:51:16 +0200

    add footer style

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="container">
-        <p className="text-center text-color-secondary text-sm font-light leading-[16.8px] md:text-[15px] md:font-normal md:leading-[22.5px] xl:text-xl xl:leading-6">&copy; Iryna Buhaichuk 2024</p>
+      <div className="container mx-auto">
+        <p className="font-roboto text-center text-color-secondary mb-5 mm:mb-3 xl:mb-6 text-xl leading-6 font-bold mm:text-2xl mm:leading-[28.8px]">Iryna Buhaichuk</p>
+        <p className="font-condensed text-center w-[238px] mm:w-full text-color-secondary mb-5 mm:mb-3 xl:mb-6 text-[13px] leading-[19.5px] font-normal mm:text-[15px] mm:leading-[22.5px] xl:text-xl xl:leading-6">Where innovation meets imagination - I transform lines of code into user experiences</p>
+        <p className="font-condensed text-center text-color-secondary text-sm font-light leading-[16.8px] mm:text-[15px] mm:leading-[22.5px] md:font-normal xl:text-xl xl:leading-6">&copy; Iryna Buhaichuk 2024</p>
       </div>
     </footer>
   );
