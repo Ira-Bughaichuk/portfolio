75f342b89d0c1f2e991e2debb1afd198c6aaaadb
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-02-11 21:40:30 +0200

    add swiper

--- src/components/ProjectSwiper/ProjectSwiper.tsx
+++ src/components/ProjectSwiper/ProjectSwiper.tsx
@@ -1,40 +1,47 @@
 "use client";
 import { Swiper, SwiperSlide } from 'swiper/react';
-// import { Pagination } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 
-import 'swiper/css';
-import 'swiper/css/pagination';
 import ProjectSlider from '../ProjectSlider/ProjectSlider';
 import { projectsList } from '@/utils/Data/projectsList';
 
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './project.css';
 
 export default function ProjectSwiper() {
   return (
-    <Swiper
-    //   modules={[ Pagination]}
-      spaceBetween={16}
-      slidesPerView={2}
-      pagination={{type:'fraction'}}
-    //   pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-    //   onSwiper={(swiper) => console.log(swiper)}
-    //   onSlideChange={() => console.log('slide change')}
-    breakpoints= {{
-        744: {
-          spaceBetween: '24px',
-          slidesPerView: '1.5'
-        },
-        1320: {
-            slidesPerView: '2.5'
+
+    <>
+      <Swiper
+      className='swiper'
+      // loop={true}
+      speed={1500}
+        modules={[ Pagination]}
+        spaceBetween={16}
+        slidesPerView={2}
+        pagination={{
+          el: '.swiper-pagination__dots',
+          clickable: true,
+        }}
+      breakpoints= {{
+          744: {
+            spaceBetween: '24px',
+            slidesPerView: '1.5'
           },
-    }}
-    >
-      {projectsList && projectsList.map((item) => (
-          <SwiperSlide key={item.id}> 
-             <ProjectSlider framework={item.framework} title={item.title} path={item.image} subTitle={item.subTitle} />
-          </SwiperSlide>
-        ))}
-    </Swiper>
+          1320: {
+              slidesPerView: '2.5'
+            },
+      }}
+      >
+        {projectsList && projectsList.map((item) => (
+            <SwiperSlide key={item.id} className='slider'> 
+              <ProjectSlider framework={item.framework} title={item.title} path={item.image} subTitle={item.subTitle} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
+      <ul className="swiper-pagination__dots"></ul> 
+    </>
+    
   )
 }
-// framework={item.framework} title={item.title} path={item.image} description={item.description} 
--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -3,26 +3,25 @@ import BtnToPage from './../../components/BtnToPage/BtnToPage';
 import ProjectSwiper from './../../components/ProjectSwiper/ProjectSwiper';
 
 
-
-
 export default function Projects() {
  
-
-  
   return (
     <section className="section">
         <div className="container mx-auto">
             <Title title='My Projects'/>
-            <div className=''>
+            <div className='flex flex-col mm:flex-row gap-5 mm:gap-6 mm:items-center'>
               <div className='flex flex-col font-condensed'>
-                <p  className='mb-6 text-xl leading-6 font-medium xl:text-4xl xl:leading-[43.2px]'>Browse Through My Works</p>
+                <p  className='mb-6 mm:w-[320px] md:w-[365px] xl:w-[536px] text-xl leading-6 font-medium xl:text-4xl xl:leading-[43.2px]'>Browse Through My Works</p>
                 <p className='mm:mb-10 text-[15px] leading-[22.5px] mm:text-[18px] mm:leading-[27px] xl:text-2xl xl:leading-9 font-normal'>Dive into my portfolio to explore a collection of projects showcasing meticulous implementation and thoughtful design.</p>
                 <div className='hidden mm:block'>
-                  <BtnToPage path={'/projects'} title={"All projects"} icon={'/*/'}/>
+                  <BtnToPage path={'/projects'} title={"All projects"} icon={'->'}/>
                 </div>
               </div>
-              <div>
+              <div className='flex flex-col gap-[18px] xl:gap-6'>
                 <ProjectSwiper />
+                <div className='mm:hidden block'>
+                  <BtnToPage path={'/projects'} title={"All projects"} icon={'->'}/>
+                </div>
               </div>
             </div>
         </div>
