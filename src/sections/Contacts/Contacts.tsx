906c8b86b6515cc0396fd98c8f070a4ac0ad46c2
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-05-21 18:13:38 +0300

    commit contact page

--- src/sections/Contacts/Contacts.tsx
+++ src/sections/Contacts/Contacts.tsx
@@ -3,9 +3,9 @@
 import { motion } from "framer-motion";
 
 import Title from './../../components/Title/Title';
-import BtnToPage from './../../components/BtnToPage/BtnToPage';
 import ContactsInfo from './../../components/ContactsInfo/ContactsInfo';
-import Square from '../../../public/icons/square.svg';
+// import Square from '../../../public/icons/square.svg';
+// import BtnToPage from './../../components/BtnToPage/BtnToPage';
 import Socials from './../../components/Socials/Socials';
 
 import { fadeInAnimationVertical } from "./../../utils/animeFunctions";
@@ -42,7 +42,8 @@ export default function Contacts() {
                    className='flex flex-col items-center font-condensed'>
                 <p className='mb-2 mm:mb-3 text-xl font-medium mm:font-bold leading-6 xl:text-2xl xl:leading-[28,8px]'>I always ready to take on your orders!</p>
                 <p className='mb-4 mm:mb-5 text-[15px] mm:text-lg font-normal leading-[22,5px] mm:leading-[27px] xl:text-2xl xl:leading-9'>Reach out to me to kickstart your project</p>
-                <motion.div 
+                
+                {/* to CONTACT PAGE  <motion.div 
                   variants={fadeInAnimationVertical} 
                   initial="initial"
                   whileInView="show"
@@ -52,7 +53,7 @@ export default function Contacts() {
                   <BtnToPage path={'/contacts'} title={'Get Started'} >
                     <Square width={24} height={24} aria-label="Square" />
                   </BtnToPage>
-                </motion.div>
+                </motion.div>  */}
                 
             </motion.div>
         </div>
--- src/utils/Data/navLink.tsx
+++ src/utils/Data/navLink.tsx
@@ -3,5 +3,5 @@ import { NavLink } from "../types";
 export const navLinks: NavLink[] = [
   { title: "Home", path: "/" },
   { title: "Projects", path: "/projects" },
-  { title: "Contacts", path: "/contacts" },
+  // { title: "Contacts", path: "/contacts" }, to CONTACT PAGE
 ];
