6882ac22f0f639701104bbfacdaa33f35020dcc8
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-06-13 18:52:33 +0300

    add button to project page

--- src/components/ProjectList/ProjectList.tsx
+++ src/components/ProjectList/ProjectList.tsx
@@ -10,7 +10,9 @@ import { fadeInAnimationHorizontalItem } from './../../utils/animeFunctions';
 
 
 export default function ProjectList({projectList}:IProjectListProps) {
-  const [tag, setTag] = useState("All");
+  console.log(projectList);
+  
+  const [tag, setTag] = useState("All projects");
 
   const handleTagChange = (newTag:string)=>{
       setTag(newTag);
@@ -22,9 +24,10 @@ export default function ProjectList({projectList}:IProjectListProps) {
   return (
     <>
       <div className='flex gap-4 xl:gap-[30px] justify-end mb-4 xl:mb-[44px]'>
-            <BtnProject  onClick={handleTagChange} tag="All" isSelected={tag === "All"}/>
-            <BtnProject  onClick={handleTagChange} tag="Com" isSelected={tag === "Com"}/>
-            <BtnProject  onClick={handleTagChange} tag="Pet" isSelected={tag === "Pet"}/> 
+            <BtnProject  onClick={handleTagChange} tag="All projects" isSelected={tag === "All projects"}/>
+            <BtnProject  onClick={handleTagChange} tag="Study projects" isSelected={tag === "Study projects"}/> 
+            <BtnProject  onClick={handleTagChange} tag="Pet projects" isSelected={tag === "Pet projects"}/>
+            <BtnProject  onClick={handleTagChange} tag="Commercial projects" isSelected={tag === "Commercial projects"}/>
       </div>
       <ul className='grid gap-[35px] md:grid-cols-2 md:gap-x-[24px] md:gap-y-[30px]'>
       {filteredProjects && filteredProjects.map((item,index) =>(
